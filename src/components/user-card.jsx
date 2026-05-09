4997a3f2fad291f5e596d774b15b67e74bd9632c
Faith Kamande <faithkamande029@gmail.com>
2026-05-09 17:40:32 +0300

    first commit

---
+++ src/components/user-card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserCard({user}) {
+  return (
+    <article>
+        <h2>{user.name}</h2>
+    </article>
+  )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import UserCard from '../components/user-card'
+
+function Home() {
+    const [users, setUser] = useState([])
+
+    useEffect(() => {
+        fetch: ("http:/localhost:4000/users")
+            .then((res) => res.json)
+            .then((data) => data.json)
+            .catch((error) => console.error(error))
+    }, [])
+
+    const userList = users.map((user) => {
+        return <UserCard key={user.id} user={user}/>
+    })
+
+  return (
+    <div>
+        <header>
+            
+        </header>
+        <main>
+            <h1>Home!</h1>
+            {userList}
+        </main>
+    </div>
+  )
+}
+
+export default Home
